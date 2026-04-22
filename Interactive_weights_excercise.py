1bae61b24bc3b213fc61e1587f72ea0a2f9bc874
HarroJongen <59564863+HarroJongen@users.noreply.github.com>
2026-04-22 18:15:50 +0200

    Milan update

--- Interactive_weights_excercise.py
+++ Interactive_weights_excercise.py
@@ -13,7 +13,7 @@ st.title("Vulnerability weights exercise")
 COMPONENT_INDICATORS = {
     "Floods": {
         "Exposure": ["Average flood depth"],
-        "Sensitivity": ["People below 6 or over 65 years", "People below 9 or over 70 years", "People below 15 or over 65 years", "Critical infrastructure"],
+        "Sensitivity": ["People below 6 or over 65 years", "People below 9 or over 70 years", "People below 15 or over 65 years", "People below 5 or over 70 years", "Critical infrastructure"],
         "Adaptive capacity": ["Household purchasing power", "Average distance to a hospital"],
     },
     "Heat": {
@@ -21,8 +21,8 @@ COMPONENT_INDICATORS = {
             "Number of hours above 30 degrees per year",
             "Number of nights exceeding 20 degrees per year",
         ],
-        "Sensitivity": ["Populaiton density", "People below 6 or over 65 years", "People below 9 or over 70 years", "People below 15 or over 65 years", "Average distance to a hospital"],
-        "Adaptive capacity": ["Household purchasing power", "People over 65 years living alone", "Blue and green fraction", "Vegetation diversity"],
+        "Sensitivity": ["Populaiton density", "People below 6 or over 65 years", "People below 9 or over 70 years", "People below 15 or over 65 years", "People below 5 or over 70 years", "Average distance to a hospital"],
+        "Adaptive capacity": ["Household purchasing power", "People over 65 years living alone", "People over 70 years living alone", "Blue and green fraction", "Vegetation diversity"],
     },
     "Disengagement from nature": {
         "Exposure": ["Distance to green"],
